7f4ed8ec789eaba64007b4a7a81cf31268e7ae81
ehebe <contact@ehebe.com>
2025-03-29 19:12:49 +0800

    Initial project

---
+++ dotfiles/nvim/lua/plugins.lua
@@ -0,0 +1,326 @@
+-- Install Lazy.nvim automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- After installation, run `checkhealth lazy` to see if everything goes right
+-- Hints:
+--     build: It will be executed when a plugin is installed or updated
+--     config: It will be executed when the plugin loads
+--     event: Lazy-load on event
+--     dependencies: table
+--                   A list of plugin names or plugin specs that should be loaded when the plugin loads.
+--                   Dependencies are always lazy-loaded unless specified otherwise.
+--     ft: Lazy-load on filetype
+--     cmd: Lazy-load on command
+--     init: Functions are always executed during startup
+--     branch: string?
+--             Branch of the repository
+--     main: string?
+--           Specify the main module to use for config() or opts()
+--           , in case it can not be determined automatically.
+--     keys: Lazy-load on key mapping (the default mode is 'n', that is normal mode)
+--           e.g., { <key mapping>, mode = {'n'}, <function> }
+--     opts: The table will be passed to the require(...).setup(opts)
+require("lazy").setup({
+	-- LSP manager
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	-- Add hooks to LSP to support Linter && Formatter
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			-- Note:
+			--     the default search path for `require` is ~/.config/nvim/lua
+			--     use a `.` as a path separator
+			--     the suffix `.lua` is not needed
+			require("config.mason-null-ls")
+		end,
+	},
+	-- Vscode-like pictograms
+	{
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "L3MON4D3/LuaSnip" },
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	{
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("config.codeium")
+		end,
+	},
+	-- Colorscheme
+	"tanvirtin/monokai.nvim",
+	-- Better UI
+	-- Run `:checkhealth noice` to check for common issues
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- Add any options here
+		},
+		dependencies = {
+			-- If you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+	-- Git integration
+	"tpope/vim-fugitive",
+	-- Git decorations
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("config.gitsigns")
+		end,
+	},
+	-- Autopairs: [], (), "", '', etc
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("config.nvim-autopairs")
+		end,
+	},
+	-- Treesitter-integration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("config.nvim-treesitter")
+		end,
+	},
+	-- Shown context when editing code
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {},
+	},
+	-- Nvim-treesitter text objects
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("config.nvim-treesitter-textobjects")
+		end,
+	},
+	-- Show indentation and blankline
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("config.indent-blankline")
+		end,
+	},
+	-- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("config.lualine")
+		end,
+	},
+	-- Markdown support
+	{ "preservim/vim-markdown", ft = { "markdown" } },
+	-- Markdown previewer
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	-- Paste image from system clipboard by :PasteImage
+	-- As suggested by the official README.md, the pngpaste shoule be ins
+	-- Requirements (MacOS): pngpaste (optional, but recommended)
+	-- Requirements (Linux): xclip (x11) or wl-clipboard (wayland)
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			default = {
+				dir_path = "assets", -- save dir
+				extension = "png",
+				file_name = "%Y-%m-%d-%H-%M-%S",
+				prompt_for_file_name = false,
+			},
+		},
+		keys = {
+			-- suggested keymap
+			{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		},
+	},
+	-- File explorer
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		config = function()
+			require("config.nvim-tree")
+		end,
+	},
+	-- Make surrounding easier
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		-- You can use the VeryLazy event for things that can
+		-- load later and are not important for the initial UI
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- Better terminal integration
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("config.toggleterm")
+		end,
+	},
+	-- Fuzzy finder
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("config.telescope")
+		end,
+	},
+	-- Improve the performance of big file
+	{
+		"pteroctopus/faster.nvim",
+	},
+	-- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+		opts = function()
+			require("config.trouble")
+		end,
+	},
+	-- Auto save
+	{
+		"okuuva/auto-save.nvim",
+		version = "*", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			-- your config goes here
+			--     -- or just leave it empty :)
+		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("which-key").setup()
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump({
+						search = { forward = true, wrap = false, multi_window = false },
+					})
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+		},
+	},
+    {
+        'Civitasv/cmake-tools.nvim',
+        opts = function ()
+			require("cmake-tools")
+        end
+    },
+})
---
+++ dotfiles/nvim/readme.md
@@ -0,0 +1,9 @@
+
+# Keymaps
+```
+    # space + e : Open diagnostics
+    # space + q : Show fix suggestions
+    # space + f : Format code
+    # ctrl + e : Open explorer
+    # ctrl + t : Open terminal
+```
